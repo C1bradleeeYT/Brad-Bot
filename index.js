ef300ccb5ed025c816f4dad4cb2962b6c2a829fb
C1bradleeeYT <42359953+C1bradleeeYT@users.noreply.github.com>
2018-08-15 21:13:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity(`with Vixels!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+bot.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '?';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}`){
+    message.channel.send("**Hello, I am Vixelated! I was actually an Human like you until I got bit by an Vixel which is a female fox and then I turned into an Vixel, after that a Tqco named Tqco_Tuesdqy found me and named me Vixelated. I now work for Vixelated which is a Minecraft Client and Web Browser, I am making for people. Sadly it is not available yet due to the fact it's not finished. Whenever it is, I will be sure to announce it to you guys!**");
+    
+const ClientId = '462054174234902541';
+
+DiscordRPC.register(ClientId);
+
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+
+rpc.on('ready', () => {
+  console.log("ready");
+  rpc.setActivity({
+    details: `Do -help if you need me.`,
+    state: 'Blowing Bubbles!',
+    // largeImageKey: 'large',
+    // largeImageText: 'Providing Vixelated',
+    // smallImageKey: 'small',
+    // smallImageText: 'Offline',
+    instance: false,
+  });
+});
+
+rpc.login(ClientId).catch(console.error);
   }
 });
 
-client.login(settings.token);
+bot.login(process.env.token);
